2afe0eb7cb1e524500257d12ed36b98c70710dae
Thomas <tom.ericdelmas@gmail.com>
2019-10-21 21:50:31 +0200

    Formattage et meilleure gestion des erreurs

--- lectureFichier.go
+++ lectureFichier.go
@@ -1,40 +1,35 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
-		"strings"
-		"strconv"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
-type coiffeur struct{
-	name string
+type coiffeur struct {
+	name           string
 	statCoupeHomme float64
 	statCoupeFemme float64
 }
 
 func main() {
-        donnees, erreur := ioutil.ReadFile("InputFile.txt")
-        lignes := strings.Split(string(donnees), "\n")
-		var attributs []string
-		var coiffeurs []coiffeur
+	donnees, erreur := ioutil.ReadFile("InputFile.txt")
+	lignes := strings.Split(string(donnees), "\n")
+	var attributs []string
+	var coiffeurs []coiffeur
 
-		for i:=0; i < len(lignes); i++ {
-			attributs = strings.Split(lignes[i], ":")
-			prenom:= attributs[0]
-			statH, err := strconv.ParseFloat(attributs[1], 64)
-			statF, err := strconv.ParseFloat(attributs[2], 64)
-			coiffeurs = append(coiffeurs, coiffeur{name : prenom, statCoupeHomme : statH, statCoupeFemme : statF})
-			
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
+	for i := 0; i < len(lignes); i++ {
+		attributs = strings.Split(lignes[i], ":")
+		prenom := attributs[0]
+		statH, _ := strconv.ParseFloat(attributs[1], 64)
+		statF, _ := strconv.ParseFloat(attributs[2], 64)
+		coiffeurs = append(coiffeurs, coiffeur{name: prenom, statCoupeHomme: statH, statCoupeFemme: statF})
+	}
 
-		fmt.Println(coiffeurs)
-		
-		if erreur != nil {
-                fmt.Println(erreur)
-		}
-		
+	fmt.Println(coiffeurs)
+
+	if erreur != nil {
+		fmt.Println("Erreur lors de la lecture du fichier")
+	}
 }
