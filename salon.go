9267b082a1b6f63b4363416c6da29601875d998b
aprouvost <aprouvost@tc405-109-13.insa-lyon.fr>
2019-10-21 18:00:16 +0200

    ajout main

--- salon.go
+++ salon.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	 "fmt"
+	 "sync"
  )
 
 
@@ -9,4 +10,5 @@ type salon struct{
 	name string
 	num_coiff int
 	waiting_line_capacity int
+	var wg sync.WaitGroup //file d'attente représentée par un waiting group
 }
