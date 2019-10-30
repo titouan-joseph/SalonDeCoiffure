9abb348097a2fbbd3921aa0383bf5709f15c67dd
Thomas <tom.ericdelmas@gmail.com>
2019-10-30 22:33:25 +0100

    Reglage d'un soucis de deadlock, amélioration du format

--- src/Main.go
+++ src/Main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-	 "fmt"
-	 "./salon"
-	 "./client"
-	 //"./coiffeur"
- )
+	"./client"
+	"./salon"
+	"fmt"
+	//"./coiffeur"
+)
 
-
- var temps_coupe_femme int //va valoir 10
- var temps_coupe_homme int //va valoir 6
+var temps_coupe_femme int //va valoir 10
+var temps_coupe_homme int //va valoir 6
 
 // fonction gérant l'arrivée d'un client dans le salon
-func client_arrival( new_client client.Client, salon salon.Salon){
-   size_waiting_line := salon.Waiting_line_capacity
-   salon.Wg.Add(1) //Ajout d'un client à la liste d'attente
+func client_arrival(new_client client.Client, salon salon.Salon) {
+	size_waiting_line := salon.Waiting_line_capacity
+	salon.Wg.Add(1) //Ajout d'un client à la liste d'attente
 }
 
-func haird_busy(new_client client.Client ){  // effectuer un time.sleep sur la goroutine du coiffeur
-    sexe := new_client.Sexe
+func haird_busy(new_client client.Client) { // effectuer un time.sleep sur la goroutine du coiffeur
+	sexe := new_client.Sexe
 }
 
 func main() {
 	//création de la slice de coiffeurs d'après InputFile.txt
 	coiffeurs := CreationCoiffeurs()
-
+	fmt.Println(coiffeurs)
 	//création de la file d'attente de clients
-	fileAttente := make(chan client.Client)
-	client1 := client.Client{Name : "Fabrice",  Sexe : "homme", Shampoo : false}
+	fileAttente := make(chan client.Client, 10)
+	client1 := client.Client{Name: "Fabrice", Sexe: "homme", Shampoo: false}
 	fileAttente <- client1
-	elt := <- fileAttente
+	elt := <-fileAttente
 	fmt.Println(elt)
 }
