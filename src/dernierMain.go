4746f41164123efa77c66a30278e59ecaeb5369e
Little-Bro <tom.ericdelmas@gmail.com>
2020-01-16 20:25:41 +0100

    clean-up, OutputFile.txt => .md

--- src/dernierMain.go
+++ src/dernierMain.go
@@ -7,15 +7,13 @@ import (
 	"os"
 	"sync"
 	"time"
-	//"io"
-
 	"./coiffeur"
 )
 
 var wg sync.WaitGroup
 
-var tempsCoupeFemme float64 = 10 //va valoir 10
-var tempsCoupeHomme float64 = 6  //va valoir 6
+var tempsCoupeFemme float64 = 10
+var tempsCoupeHomme float64 = 6  
 var tempsShampoo float64 = 15
 
 var coiffeursLibres []coiffeur.Coiffeur
@@ -49,14 +47,13 @@ func temps_process(new_client *client.Client, new_haid *coiffeur.Coiffeur) float
 }
 
 //----- Fonction qui retire un élément d'une liste ---
-func remove(s []coiffeur.Coiffeur, i int) []coiffeur.Coiffeur {   // Besoin?
+func remove(s []coiffeur.Coiffeur, i int) []coiffeur.Coiffeur { 
 	s[i] = s[len(s)-1]
 	return s[:len(s)-1]
 }
 
 // ------ Fonction servant à placer le coiffeur dans les bonnes listes pour  la réalisation de la coupe
 //		   sélectionne celui qui s'occupe du client -----
-
 func haird_busy( coiffeursLibres chan coiffeur.Coiffeur, coiffeursOccupes chan coiffeur.Coiffeur ) coiffeur.Coiffeur {
 
 	coiff_occupe := <- coiffeursLibres
@@ -66,14 +63,8 @@ func haird_busy( coiffeursLibres chan coiffeur.Coiffeur, coiffeursOccupes chan c
 }
 
 // ------ Fonction servant à placer le coiffeur dans les bonnes listes après la réalisation de la coupe -----
-
 func haird_not_busy(new_haird coiffeur.Coiffeur, coiffeursLibres chan coiffeur.Coiffeur, coiffeursOccupes chan coiffeur.Coiffeur) {
-
-	// Ecriture dans le fichier texte du client et des caractéristiques
-	//EcritureClient(new_client, new_haird)
-
 	for i := 0; i < len(coiffeursOccupes) ; i++ {
-
 		coiff_retire_channel := <- coiffeursOccupes
 		if coiff_retire_channel == new_haird {
 			new_haird.Libre = true
@@ -86,21 +77,18 @@ func haird_not_busy(new_haird coiffeur.Coiffeur, coiffeursLibres chan coiffeur.C
 }
 
 //  ----- Fonction servant à terminer la simulation -----
-
 func end_of_day() time.Duration {
 
 	endTimer := time.Now()                      // arret du timer
 	timeOfExecution := endTimer.Sub(startTimer) // calcul du temps
-	// fermer ecriture du fichier et imprime le fichier
 	return timeOfExecution
 }
 
-func deleteFile( path string) {
-	// delete file
+// ----- Fonction servant à supprimer le dernier OutputFile.md -----
+func deleteFile(path string) {
 	var err = os.Remove(path)
 	if err != nil {
 		{
-			// handle error
 			fmt.Printf("Could not create listener\n")
 			panic(err)
 		}
@@ -131,7 +119,7 @@ func operation(new_client *client.Client, new_haird *coiffeur.Coiffeur, coiffeur
 	duration := time.Duration(temps_process(new_client, new_haird))
 	EcritureClient(new_client, new_haird)
 	fmt.Println(new_haird, "  prend en charge  ", new_client, " en temps: ", duration)
-	time.Sleep(duration*time.Second) // effectue un équivalent de time.sleep sur la goroutine
+	time.Sleep(duration*time.Second) 
 	wg.Done()
 	haird_not_busy(*new_haird, coiffeursLibres, coiffeursOccupes)
 	fmt.Println(new_haird, "  a fini avec  ", new_client)
@@ -141,32 +129,30 @@ func operation(new_client *client.Client, new_haird *coiffeur.Coiffeur, coiffeur
 // ----- Fonction Main du projet -----
 func test() {
 
-	if _, err := os.Stat("OutputFile.txt"); err == nil {
+	if _, err := os.Stat("OutputFile.md"); err == nil {
 		// path/to/whatever exists
-		deleteFile("OutputFile.txt")
+		deleteFile("OutputFile.md")
 
 	} else if os.IsNotExist(err) {
 		// path/to/whatever does *not* exist
-		fmt.Print(" No file names as OutputFile.txt for the moment")
+		fmt.Print(" No file names as OutputFile.md for the moment")
 
 	} else {
 		// Schrodinger: file may or may not exist. See err for details.
 		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
 	}
 
-	createFile("OutputFile.txt")
+	createFile("OutputFile.md")
 
-
-
-	nombreClients := 8 // Simulation à n clients
-	nombreCoiffeurs := 4 // Simulation à 4 coiffeurs, attention prendre loe meme nombre que dans le fichier texte
+	nombreClients := 8 
+	nombreCoiffeurs := 4 // Simulation à 4 coiffeurs, attention prendre le meme nombre que dans le fichier texte
 
 	fileAttente := make(chan client.Client, nombreClients) //création de la file d'attente de clients
 	fileCoiffeursLibres := make(chan coiffeur.Coiffeur, nombreCoiffeurs)
 	fileCoiffeursOccupes := make(chan coiffeur.Coiffeur, nombreCoiffeurs)
 
 	fmt.Println("Creation file d'attente ")
-	coiffeursLibres := CreationCoiffeurs()          //création de la liste de coiffeurs d'après InputFile.txt
+	coiffeursLibres := CreationCoiffeurs()  //création de la liste de coiffeurs d'après InputFile.txt
 	fmt.Println("Creation liste coiffeurs ")
 	listeClients := CreationClients()
 	fmt.Println("Creation liste clients ")
@@ -182,16 +168,13 @@ func test() {
 		fileCoiffeursLibres <- coiffeursLibres[i]
 	}
 
-
-	for len(fileAttente)!= 0  { //equivalent du while qui tourne pendant toute l'execution du programme
-
-		clientOccupe := <-fileAttente                       // retire un client de la file d'attente
-		newHaird := haird_busy(fileCoiffeursLibres, fileCoiffeursOccupes)                            // choisit quel coiffeur s'en occupe
-		go operation(&clientOccupe, &newHaird, fileCoiffeursLibres, fileCoiffeursOccupes)
-
+	//boucle qui tourne pendant toute l'execution du programme
+	for len(fileAttente)!= 0  { 		
+		clientOccupe := <-fileAttente  // retire un client de la file d'attente
+		newHaird := haird_busy(fileCoiffeursLibres, fileCoiffeursOccupes) // choisit quel coiffeur s'en occupe
+		go operation(&clientOccupe, &newHaird, fileCoiffeursLibres, fileCoiffeursOccupes) // lancement de l'opération
 	}
 
-	//fmt.Println("coiffeurs :", coiffeurs)
 	fmt.Println("nombre coiffeurs libres :", len(fileCoiffeursLibres))
 
 	wg.Wait() //empêche le programme de terminer avant les go-routines
